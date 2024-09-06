6973796d80c2d281d9de56281d73c0b4e9901fc9
Kayla K <105766386+k-kenney@users.noreply.github.com>
2024-09-06 08:23:57 -0500

    project set up with firebase

---
+++ src/config/firebase-config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth'
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwM25jIgB2H7mH7MCeTTEFVJnNymvEe_Q",
+  authDomain: "expense-tracker-e91b8.firebaseapp.com",
+  projectId: "expense-tracker-e91b8",
+  storageBucket: "expense-tracker-e91b8.appspot.com",
+  messagingSenderId: "840875982038",
+  appId: "1:840875982038:web:08b8b82928dfc0c7505d77"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
